2978e1a8b102acc1ca773794b6c05fc81524f29e
farleybsd <farley.t.i@hotmail.com>
2020-08-17 20:24:33 -0300

    var_dump com o Xdebug

--- var_dump.php
+++ var_dump.php
@@ -1,25 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
 
 
 
-<?php
+
+    <?php
 
 
-use Alura\Leilao\Model\Leilao;
+    use Alura\Leilao\Model\Leilao;
 
-require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 
-$usuario =  new Leilao('Fiat 147 0Km');
-//var_dump('<pre>',$usuario);
-phpinfo();
-?>
+    $usuario =  new Leilao('Fiat 147 0Km');
+    //var_dump('<pre>', $usuario);
+    var_dump($usuario);
+    //phpinfo();
+    ?>
 </body>
+
 </html>
